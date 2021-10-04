5b15a3f6b45d7a8fe1e5b3d36a2665ad3f9d0244
Vivek Choudhary <61905961+vivek-12001@users.noreply.github.com>
2021-10-04 14:59:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> I am Vivek Choudhary - 22202 from Quantiphi - Manual Invocation.</h1>");
+  response.end("<h1> I am Vivek Choudhary - 22202 from Quantiphi - Pub Sub.</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
